c63341d61727f0402db4134b3971e3d0a01e19bd
StutiSk <stuti.kumari@leafround.com>
2024-06-14 12:03:28 +0530

    fixed

--- src/HTMLtemplates/Templates.js
+++ src/HTMLtemplates/Templates.js
@@ -19,11 +19,11 @@
 
 
 export const Templates = {
-  template1:`<div class="Slide16913" style="width: 1080px; height: 1299px; background: linear-gradient(0deg, black 0%, black 100%); background: url(https://via.placeholder.com/1080x1299) center center/contain ; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
+  template1:`<div class="Slide16913" style="width: 1080px; height: 1299px; background: linear-gradient(0deg, black 0%, black 100%); background: url(https://via.placeholder.com/1080x1299) center center/contain; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
   <div class="Top" style="align-self: stretch; flex: 1 1 0; padding-top: 120px; padding-bottom: 48px; flex-direction: column; justify-content: flex-end; align-items: center; gap: 10px; display: flex">
     <img class="Logo" style="width: 176.50px; height: 77.50px" src="https://via.placeholder.com/176x77" />
   </div>
-  <div class="Middle" style="align-self: stretch; height: 572px; padding: 128px; flex-direction: column; justify-content: center; align-items: center; gap: 36px; display: flex">
+  <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: center; align-items: center; gap: 36px; display: flex">
     <div class="PrimaryText" style="align-self: stretch; text-align: center; color: white; font-size: 82.06px; font-family: Inter; font-weight: 600; line-height: 118.99px; word-wrap: break-word">Earn upto 14% IRR<br/>in 62 days</div>
     <div class="SecondaryText" style="text-align: center; color: white; font-size: 29.99px; font-family: Inter; font-weight: 400; line-height: 41.98px; word-wrap: break-word">with a new Invoice Discounting Deal</div>
   </div>
