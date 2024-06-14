9dd09af37fc1a456c5f8919dd0d59df2a836a965
StutiSk <stuti.kumari@leafround.com>
2024-06-14 15:56:23 +0530

    fix

--- src/HTMLtemplates/Templates.js
+++ src/HTMLtemplates/Templates.js
@@ -18,23 +18,22 @@
 // }
 
 export const Templates = {
-  template1: `<div class="Slide16915" style="
-width: 1080px; height: 1299px; background: linear-gradient(0deg, black 0%, black 100%); background: url(https://via.placeholder.com/1080x1299) center center/contain; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
-  <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: flex-end; align-items: center; gap: 180px; display: flex">
-    <img class="Logo" style="width: 176.50px; height: 77.50px" src="https://via.placeholder.com/176x77" />
-  </div>
-  <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: center; align-items: center; gap: 180px; display: flex">
-    <div class="Frame1116602857" style="align-self: stretch; height: 316px; flex-direction: column; justify-content: center; align-items: center; gap: 36px; display: flex">
-      <div class="PrimaryText" style="align-self: stretch; text-align: center; color: white; font-size: 82.06px; font-family: Inter; font-weight: 600; line-height: 118.99px; word-wrap: break-word">Earn upto 14% IRR in 62 days</div>
-      <div class="SecondaryText" style="text-align: center; color: white; font-size: 29.99px; font-family: Inter; font-weight: 400; line-height: 41.98px; word-wrap: break-word">with a new Invoice Discounting Deal</div>
+  template1: `<div class="Slide16915" style="width: 1080px; height: 1299px; background: linear-gradient(0deg, black 0%, black 100%); background: url(https://via.placeholder.com/1080x1299) center center/contain; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
+    <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: flex-end; align-items: center; gap: 180px; display: flex">
+      <img class="Logo" style="width: 176.50px; height: 77.50px" src="https://via.placeholder.com/176x77" />
     </div>
-  </div>
-  <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: center; align-items: center; gap: 180px; display: flex">
-    <div class="Button" style="padding-left: 47.69px; padding-right: 47.69px; padding-top: 33.17px; padding-bottom: 33.17px; background: white; box-shadow: 0px 2.6228957176208496px 4.292011260986328px -0.7153352499008179px rgba(0, 0, 0, 0.10); border-radius: 1380.52px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
-      <div class="TextLayer" style="padding-left: 19.08px; padding-right: 19.08px; justify-content: center; align-items: center; gap: 23.84px; display: flex">
-        <div class="ButtonText" style="color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Read More</div>
+    <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding-left: 120px; padding-right: 120px; padding-top: 48px; padding-bottom: 48px; flex-direction: column; justify-content: center; align-items: center; gap: 180px; display: flex">
+      <div class="Frame1116602857" style="align-self: stretch; height: 316px; flex-direction: column; justify-content: center; align-items: center; gap: 36px; display: flex">
+        <div class="PrimaryText" style="align-self: stretch; text-align: center; color: white; font-size: 82.06px; font-family: Inter; font-weight: 600; line-height: 118.99px; word-wrap: break-word">Earn upto 14% IRR in 62 days</div>
+        <div class="SecondaryText" style="text-align: center; color: white; font-size: 29.99px; font-family: Inter; font-weight: 400; line-height: 41.98px; word-wrap: break-word">with a new Invoice Discounting Deal</div>
       </div>
     </div>
-  </div>
-</div>`,
+    <div class="Middle" style="align-self: stretch; flex: 1 1 0; padding: 48px; flex-direction: column; justify-content: center; align-items: center; gap: 180px; display: flex">
+      <div class="Button" style="padding-left: 47.69px; padding-right: 47.69px; padding-top: 33.17px; padding-bottom: 33.17px; background: white; box-shadow: 0px 2.6228957176208496px 4.292011260986328px -0.7153352499008179px rgba(0, 0, 0, 0.10); border-radius: 1380.52px; overflow: hidden; justify-content: center; align-items: center; display: inline-flex">
+        <div class="TextLayer" style="padding-left: 19.08px; padding-right: 19.08px; justify-content: center; align-items: center; gap: 23.84px; display: flex">
+          <div class="ButtonText" style="color: black; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Read More</div>
+        </div>
+      </div>
+    </div>
+  </div>`,
 };
